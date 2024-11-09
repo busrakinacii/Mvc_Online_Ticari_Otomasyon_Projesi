ef2326124fe0aeb8fc24a55dc20ab481356cc4ec
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-09 20:45:49 +0300

    Bill And Department classes Added

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Bill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class Bill
+    {
+        //Bill=Fatura
+        [Key]
+        public int BillId { get; set; }
+        public string BillSerialNumber { get; set; }
+        public string BillOrderNumber { get; set; }
+        public DateTime BillDatetime { get; set; }
+
+        //Vergi Dairesi=Tax Office
+        public string BillTaxOffice { get; set; }
+
+        public DateTime BillClock { get; set; }
+        //Deliverer=Teslim Eden
+        public string BillDeliverer { get; set; }
+        //Teslim Alan=recipient
+        public string BillRecipient { get; set; }
+
+    }
+}

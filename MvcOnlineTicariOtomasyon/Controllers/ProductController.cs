86c69a745810d196b85cf67a8466aaa221c0fb00
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-13 22:24:21 +0300

    Changes Added

--- MvcOnlineTicariOtomasyon/Controllers/ProductController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ProductController.cs
@@ -13,18 +13,32 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context co = new Context();
         public ActionResult Index()
         {
-            var pro = co.Products.ToList();
+            var pro = co.Products.Where(x => x.ProductStatus == true).ToList();
             return View(pro);
         }
         [HttpGet]
         public ActionResult ProductAdd()
         {
+            List<SelectListItem> listValue = (from x in co.Categories.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.CategoryName,
+                                                  Value = x.CategoryId.ToString()
+                                              }).ToList();
+            ViewBag.Val = listValue;
             return View();
         }
         [HttpPost]
         public ActionResult ProductAdd(Product p)
         {
             var pro = co.Products.Add(p);
+            co.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult ProductDelete(int id)
+        {
+            var value = co.Products.Find(id);
+            value.ProductStatus = false;
             co.SaveChanges();
             return RedirectToAction("Index");
         }

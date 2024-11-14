ebdae541d15977f44e46e5a7361fd60a41ac3f92
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-14 21:00:52 +0300

    Department controller and index pages Added

---
+++ MvcOnlineTicariOtomasyon/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        Context co = new Context();
+        public ActionResult Index()
+        {
+            var values = co.Departments.ToList();
+            return View(values);
+        }
+    }
+}

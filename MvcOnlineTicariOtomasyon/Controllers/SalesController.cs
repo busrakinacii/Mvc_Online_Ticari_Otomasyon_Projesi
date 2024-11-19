722107fb65e7a7baf7db633156459792bab3d081
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-19 22:49:47 +0300

    Sales Controller page Added

---
+++ MvcOnlineTicariOtomasyon/Controllers/SalesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        Context co = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

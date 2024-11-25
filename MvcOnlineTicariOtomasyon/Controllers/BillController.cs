ddd02aee3888928fda2f63fa0559bef5207d9c32
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-11-25 21:19:50 +0300

    BillAdd Page Added

--- MvcOnlineTicariOtomasyon/Controllers/BillController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/BillController.cs
@@ -16,5 +16,17 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var list = co.Bills.ToList();
             return View(list);
         }
+        [HttpGet]
+        public ActionResult BillAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult BillAdd(Bill b)
+        {
+            co.Bills.Add(b);
+            co.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

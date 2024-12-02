b9138f10a89e0d02d312ebb84da22dafd80a933b
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-02 22:01:09 +0300

    Changes Added

--- MvcOnlineTicariOtomasyon/Controllers/GalleryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/GalleryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class GalleryController : Controller
     {
         // GET: Gallery
+        Context co = new Context();
         public ActionResult Index()
         {
-            return View();
+            var value = co.Products.ToList();
+            return View(value);
         }
     }
 }

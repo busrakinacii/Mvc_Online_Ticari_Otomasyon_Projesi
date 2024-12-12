018f2ea4ffd519663e384ce976dba9427358d55c
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-12 23:12:29 +0300

    changes added

--- MvcOnlineTicariOtomasyon/Controllers/ToDoListController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ToDoListController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class ToDoListController : Controller
     {
+        Context co = new Context();
         // GET: ToDoList
         public ActionResult Index()
         {

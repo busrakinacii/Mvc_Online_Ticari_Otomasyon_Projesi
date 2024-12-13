73fb7f24b92659838c33af81bee9e1b3a3e71926
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-13 22:46:21 +0300

    ToDoList Controller values added

--- MvcOnlineTicariOtomasyon/Controllers/ToDoListController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ToDoListController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using MvcOnlineTicariOtomasyon.Models.Classes;
 
 
@@ -14,6 +15,14 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         // GET: ToDoList
         public ActionResult Index()
         {
+            var value1 = co.Currents.Count().ToString();
+            ViewBag.v1 = value1;
+            var value2 = co.Products.Count().ToString();
+            ViewBag.v2 = value2;
+            var value3 = co.Categories.Count().ToString();
+            ViewBag.v3 = value3;
+            var value4 = (from x in co.Currents select x.CurrentCity).Distinct().Count().ToString();
+            ViewBag.v4 = value4;
             return View();
         }
     }

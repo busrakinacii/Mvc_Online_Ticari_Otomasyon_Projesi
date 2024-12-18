3149a8ced69834a74d94764bf67d9d7aa74e1dc3
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-18 22:45:33 +0300

    Login page changes Added

--- MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -26,6 +27,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             co.Currents.Add(crnt);
             co.SaveChanges();
             return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CurrentLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CurrentLogin1(Current crnt)
+        {
+            var information = co.Currents.FirstOrDefault(x => x.CurrentMail == crnt.CurrentMail && x.CurrentPassword == crnt.CurrentPassword);
+            if (information != null)
+            {
+                FormsAuthentication.SetAuthCookie(information.CurrentMail, false);
+                Session["CurrentMail"] = information.CurrentMail.ToString();
+                return RedirectToAction("Index", "CurrentPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+
+            }
+
         }
     }
 }

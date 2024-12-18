3e7d40ff25919d00ebac4be9a2179b98eb1fd07a
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-18 21:39:10 +0300

    Some Pages Added

---
+++ MvcOnlineTicariOtomasyon/Controllers/CurrentPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CurrentPanelController : Controller
+    {
+        // GET: CurrentPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

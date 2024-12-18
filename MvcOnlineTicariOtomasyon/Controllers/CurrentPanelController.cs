2e9e66a02fec8b61491230d838f1283b5d5a3923
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-18 22:52:17 +0300

    Session with mail transport

--- MvcOnlineTicariOtomasyon/Controllers/CurrentPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CurrentPanelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,15 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class CurrentPanelController : Controller
     {
         // GET: CurrentPanel
+        Context co = new Context();
         [Authorize]
+
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CurrentMail"];
+            var values = co.Currents.FirstOrDefault(x => x.CurrentMail == mail);
+            ViewBag.m = mail;
+            return View(values);
         }
     }
 }

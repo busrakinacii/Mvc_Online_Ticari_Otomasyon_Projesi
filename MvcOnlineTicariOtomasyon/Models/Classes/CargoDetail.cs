dc69fb038fa4629e0291e63d770fb15d73728d8a
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-31 15:37:26 +0300

    cargoDetail class page added

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/CargoDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class CargoDetail
+    {
+        public int CargoDetailID { get; set; }
+        public string Comment { get; set; }
+        public string TrackingCode { get; set; }
+        public string Personnel { get; set; }
+        //Buyer=Alıcı
+        public string Buyer { get; set; }
+        public DateTime Datetime { get; set; }
+    }
+}

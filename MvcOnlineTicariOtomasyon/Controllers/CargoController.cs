00f468786a33d22140d67e2e38a71f6aea8aef1e
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-01-02 23:20:26 +0300

    Random Added and Changed Added

--- MvcOnlineTicariOtomasyon/Controllers/CargoController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CargoController.cs
@@ -19,6 +19,18 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpGet]
         public ActionResult CargoAdd()
         {
+            Random rnd = new Random();
+            string[] character = { "A", "B", "C", "D", "E", "F", "G", "H", "K" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, character.Length);
+            k2 = rnd.Next(0, character.Length);
+            k3 = rnd.Next(0, character.Length);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);//10-->3 1 2 1 2 1 
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+            string code = s1.ToString() + character[k1] + s2.ToString() + character[k2] + s3.ToString() + character[k3];
+            ViewBag.trackingCode = code;
             return View();
         }
         [HttpPost]

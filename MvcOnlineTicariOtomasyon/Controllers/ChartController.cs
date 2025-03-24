465a1789384a6c45537bb23eeeb84275a710a624
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-03-24 17:13:37 +0300

    Chart Views Added

--- MvcOnlineTicariOtomasyon/Controllers/ChartController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ChartController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
@@ -13,5 +14,15 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         {
             return View();
         }
+
+        public ActionResult Index2()
+        {
+            var chartDraw = new Chart(600, 600);
+            chartDraw.AddTitle("Kategori - Ürün Stojk Sayısı").AddLegend("Stok").AddSeries(
+                "Değerler", xValue: new[] { "Mobilya", "Ofis Eşyaları", "Bilgisayar" }, yValues: new[] { 85, 66, 98 }).Write();
+            return File(chartDraw.ToWebImage().GetBytes(), "image/jpeg");
+
+        }
+
     }
 }

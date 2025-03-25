019fa4f07ddc246cb99cdc89fb58ecda68dcbb2a
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-03-25 16:26:28 +0300

    changed Added

--- MvcOnlineTicariOtomasyon/Controllers/ChartController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +25,18 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             return File(chartDraw.ToWebImage().GetBytes(), "image/jpeg");
 
         }
-
+        Context co = new Context();
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var results = co.Products.ToList();
+            results.ToList().ForEach(x => xvalue.Add(x.ProductName));
+            results.ToList().ForEach(y => yvalue.Add(y.ProductStock));
+            var chart = new Chart(width: 850, height: 850)
+                .AddTitle("Stoklar")
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(chart.ToWebImage().GetBytes(), "image/jpeg");
+        }
     }
 }

8295b7c77e49e31c7d213ad37379f04261a721b1
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-05-12 22:09:01 +0300

    changed added and class2 page added,categoryController page added

--- MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -50,5 +50,29 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             co.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Test()
+        {
+            Class2 cs = new Class2();
+            cs.Categories = new SelectList(co.Categories, "CategoryId", "CategoryName");
+            cs.Products = new SelectList(co.Products, "ProductId", "ProductName");
+            return View(cs);
+        }
+
+        //json Deger olarak Kategoriye göre seçilen ürünleri listeleme işlem yaptık.
+        // Asp.Net Mvc5 ile Online Ticari Otomasyon dersi video 247
+
+        public JsonResult ProductBring(int p)
+        {
+            var productList = (from x in co.Products
+                               join y in co.Categories
+                               on x.Category.CategoryId equals y.CategoryId
+                               where x.Category.CategoryId == p
+                               select new
+                               {
+                                   Text = x.ProductName,
+                                   Value = x.ProductId.ToString()
+                               }).ToList();
+            return Json(productList,JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class Class2
+    {
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<SelectListItem> Products { get; set; }
+    }
+}
